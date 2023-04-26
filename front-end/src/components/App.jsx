d4e71545cd198098a7e9cc64554f5249f9681d48
drianodev <adrianomendes661@gmail.com>
2023-04-26 11:31:49 -0300

    first commit

---
+++ front-end/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './Header';
+import NovoUsuario from './NovoUsuario';
+import Toast from './Toast';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <NovoUsuario
+                    erro={msg => this.refs.toast.erro(msg)}    
+                />
+                <Toast ref="toast" />
+            </div>
+        );
+    }
+}
+
+export default App;
